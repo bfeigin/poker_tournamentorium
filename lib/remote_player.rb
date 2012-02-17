6ce334356f8d4b3576f08b0425bd078346af66cf
Robert Nubel <robert.nubel@gmail.com>
2012-02-17 00:29:26 -0600

    Adds tests for the remote seating call.

--- lib/remote_player.rb
+++ lib/remote_player.rb
@@ -1,6 +1,13 @@
 # Module to include into the Player model.
 module RemotePlayer
+  def seating_path
+    "#{self.hostname}/seating"
+  end
+
   def accepts_seat?(table)
+    RestClient.post(seating_path, :params => { :table => table.as_json }) do |response, request, result|
+      response.code == 200
+    end
   end
 
   def get_action(round)
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -14,4 +14,31 @@ describe "Player Model" do
 
     player.game_tables.current_table.should == table
   end
+
+  describe "remote actions" do
+    before :each do
+      @player = Factory.create(:player)
+      @table = Factory.create(:game_table)
+      @player.hostname = "testhost"
+      
+    end
+
+    it "should seat the player if the client returns a 200 from a POST to hostname/seating" do
+      FakeWeb.register_uri(:post, "testhost/seating",
+            { :body => "", :status => ["200", "Okay"] })
+
+      @player.seat(@table)
+
+      @player.game_tables.current_table.should == @table
+    end
+
+    it "should not seat the player if the client returns a 403" do
+      FakeWeb.register_uri(:post, "testhost/seating",
+            { :body => "", :status => ["403", "Not Gonna Bother"] })
+
+      @player.seat(@table)
+
+      @player.game_tables.current_table.should be_nil
+    end
+  end
 end
