db39253b61fd61207b187a663594e967b0be1978
Ben Feigin <bfeigin@enova.com>
2012-02-24 11:24:41 -0600

    I do not work for the library, it works for me

--- models/cards.rb
+++ models/cards.rb
@@ -2,44 +2,24 @@ class Card < ActiveRecord::Base
   belongs_to :dealable, :polymorphic => true
   belongs_to :hand, :foreign_key => 'hand_id'
 
-
-  def to_s
-    (value_code.to_s + suit_code.to_s).downcase
-  end
-
-  def <=> other_card
-    face <=> other_card.face
-  end
-  
-  #def is_a?(klass)
-  #  klass == Card || self === klass
-  #end
-
-  def == other_card
-    face == other_card.face && suit_code == other_card.suit_code
-  end
-  alias :eql? :==
-
-  def face
+  def face_value
     self.class.face_value(value_code)
-  end
-  alias :face_value :face
-
-  def suit
-    case suit_code.downcase
-    when 'c'
-      0
-    when 'd'
-      1
-    when 'h'
-      2
-    when 's'
-      3
-    end
   end
 
   def suit_name
-    case suit_code.upcase
+    self.class.suit_name(suit_code)
+  end
+
+  # Return just the basic info.
+  def as_json(opts={})
+    {
+      :suit => suit_code.to_s,
+      :value => value_code.to_s
+    }
+  end
+
+  def self.suit_name
+    case suit.upcase
     when 'C'
       'Clubs'
     when 'D'
@@ -49,14 +29,6 @@ class Card < ActiveRecord::Base
     when 'S'
       'Spades'
     end
-  end
-
-  # Return just the basic info.
-  def as_json(opts={})
-    {
-      :suit => suit_code.to_s,
-      :value => value_code.to_s
-    }
   end
 
   def self.face_value(value)
